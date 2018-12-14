a3ae35d7f0bee2ac18fe400471b728694a995e1e
Dean Radcliffe <deanmisc@yahoo.com>
2018-12-14 15:26:47 -0600

    build: Update build

---
+++ book-stream/build/precache-manifest.1433dca3a665b3b5f11176ee063c021d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a5b7fc431098824323d4",
+    "url": "/static/js/main.a5b7fc43.chunk.js"
+  },
+  {
+    "revision": "cc917451e4fa98a40253",
+    "url": "/static/js/1.cc917451.chunk.js"
+  },
+  {
+    "revision": "a5b7fc431098824323d4",
+    "url": "/static/css/main.9a329feb.chunk.css"
+  },
+  {
+    "revision": "223f0b12ef6f8a0c24157d20be3dd85b",
+    "url": "/index.html"
+  }
+];
--- book-stream/build/service-worker.js
+++ book-stream/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af1380f0bc0cf38782fa1adac546b1b1.js"
+  "/precache-manifest.1433dca3a665b3b5f11176ee063c021d.js"
 );
 
 workbox.clientsClaim();
