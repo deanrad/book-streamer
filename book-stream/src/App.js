31c1b218cac9829ecbaf67876df51e3a38f22b33
Dean Radcliffe <deanmisc@yahoo.com>
2018-12-14 11:33:17 -0600

    feat: Complete a rudimentary UX

--- book-stream/src/App.js
+++ book-stream/src/App.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
+class App extends PureComponent {
   render() {
+    const { q, loading, results } = this.props;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <input id="q" defaultValue={q} style={{ fontSize: "2em" }} />
+          {loading && "⏳"}
+        </div>
+        <ul>
+          {(results || []).map(volume => {
+            const info = volume.volumeInfo || {};
+            const { title, publisher, canonicalVolumeLink: link } = info;
+            const thumb = (info.imageLinks || {}).smallThumbnail;
+            const authors = (info.authors || []).join(", ");
+
+            return (
+              <li>
+                <a href={link} target="_blank" rel="noopener">
+                  <img height="50" src={thumb} alt={title} />
+                  {title} - {authors} - {publisher}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
